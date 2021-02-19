c04b8f2f3210bdc3947eba5d150e14aa584bff1c
moabre <muhammadrehman21@gmail.com>
2021-02-18 18:35:00 -0600

    creating the apollo connection with graphql server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+import "./index.css";
+import Chat from './Chat'
+
+const App = () => <Chat/>
 
 ReactDOM.render(<App />, document.getElementById("app"));

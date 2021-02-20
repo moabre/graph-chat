44525522a0d752e11154395f5c44b4e234337f2b
moabre <muhammadrehman21@gmail.com>
2021-02-20 14:38:57 -0600

    added the subscription to both client and server side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
+import Chat from './Chat';
 
-import "./index.css";
-import Chat from './Chat'
+const App = () => <Chat />;
 
-const App = () => <Chat/>
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,37 +1,160 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+  useMutation,
+} from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const link = new WebSocketLink({
+  uri: `ws://localhost:4000/`,
+  options: {
+    reconnect: true,
+  },
+});
 
 const client = new ApolloClient({
+  link,
   uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
+
 const GET_MESSAGES = gql`
-query{
-  messages{
-    id
-    content
-    user
+  query {
+    messages {
+      id
+      content
+      user
+    }
   }
-}
 `;
 
-const Messages = ({user}) => {
- const {data} = useQuery(GET_MESSAGES);
- if (!data){
-  return null;
- }
+const POST_MESSAGE = gql`
+  mutation($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
+  }
+`;
 
- return JSON.stringify(data)
-}
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+  if (!data) {
+    return null;
+  }
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em',
+          }}
+        >
+          {user !== messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: '0.5em',
+                border: '2px solid #e5e6ea',
+                borderRadius: 25,
+                textAlign: 'center',
+                fontSize: '18pt',
+                paddingTop: 7,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? '#58bf56' : '#e5e6ea',
+              color: user === messageUser ? '#white' : 'black',
+              padding: '1em',
+              borderRadius: '1em',
+              maxWidth: '60%',
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 const Chat = () => {
- return(
-  <div><Messages user = 'AR'/> </div>
- )
-}
+  const [name, setName] = React.useState({
+    user: 'AR',
+    content: '',
+  });
+  const [postMessage] = useMutation(POST_MESSAGE);
+
+  const onSend = () => {
+    if (name.content.length > 0) {
+      postMessage({
+        variables: name,
+      });
+    }
+    setName({
+      ...name,
+      content: '',
+    });
+  };
+
+  return (
+    <Container>
+      <Messages user={name.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0 }}>
+          <Form.Control
+            label='User'
+            value={name.user}
+            onChange={(e) =>
+              setName({
+                ...name,
+                user: e.target.value,
+              })
+            }
+          />
+        </Col>
+        <Col xs={8}>
+          <Form.Control
+            label='Content'
+            value={name.content}
+            onChange={(e) =>
+              setName({
+                ...name,
+                content: e.target.value,
+              })
+            }
+            onKeyUp={(e) => {
+              if (e.key === 13) {
+                onSend();
+              }
+            }}
+          />
+        </Col>
+        <Col xs={2} style={{ padding: 0 }}>
+          <Button onClick={() => onSend()} style={{ width: '100%' }}>
+            Send
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default () => (
- <ApolloProvider client = {client}>
-  <Chat />
- </ApolloProvider>
-)
+  <ApolloProvider client={client}>
+    <Chat />
+  </ApolloProvider>
+);

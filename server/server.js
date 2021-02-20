88fb02e6d8b43870e5256780e52efcbcc63c0949
moabre <muhammadrehman21@gmail.com>
2021-02-20 16:38:31 -0600

    attempt at resolving the port issue for heroku

--- server/server.js
+++ server/server.js
@@ -56,6 +56,16 @@ const resolvers = {
 const pubsub = new PubSub();
 const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+// server.start({ port: process.env.PORT || 4000 })=> {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
+
+const options = {
+  port: process.env.PORT || 4000,
+};
+
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`
+  )
+);
